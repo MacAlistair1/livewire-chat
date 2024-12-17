2ae344c66ecc13643188b33ff1127a6202058bf1
MacAlistair1 <lamichhaneaj@gmail.com>
2024-12-17 14:21:39 +0545

    Live Chat with live wire

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,17 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Support\Collection;
+use Namu\WireChat\Traits\Chatable;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
+    use Chatable;
 
     /**
      * The attributes that are mass assignable.
@@ -44,5 +47,64 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /**
+     * Returns the URL for the user's cover image (avatar).
+     * Adjust the 'avatar_url' field to your database setup.
+     */
+    public function getCoverUrlAttribute(): ?string
+    {
+        return $this->avatar_url ?? null;
+    }
+
+    /**
+     * Returns the URL for the user's profile page.
+     * Adjust the 'profile' route as needed for your setup.
+     */
+    public function getProfileUrlAttribute(): ?string
+    {
+        return route('profile', ['id' => $this->id]);
+    }
+
+    /**
+     * Returns the display name for the user.
+     * Modify this to use your preferred name field.
+     */
+    public function getDisplayNameAttribute(): ?string
+    {
+        return $this->name ?? 'user';
+    }
+
+    /**
+     * Search for users when creating a new chat or adding members to a group.
+     * Customize the search logic to limit results, such as restricting to friends or eligible users only.
+     */
+    public function searchChatables(string $query): ?Collection
+    {
+        $searchableFields = ['name'];
+        return User::where(function ($queryBuilder) use ($searchableFields, $query) {
+            foreach ($searchableFields as $field) {
+                $queryBuilder->orWhere($field, 'LIKE', '%' . $query . '%');
+            }
+        })
+            ->limit(20)
+            ->get();
+    }
+
+    /**
+     * Determine if the user can create new groups.
+     */
+    public function canCreateGroups(): bool
+    {
+        return $this->hasVerifiedEmail() == false;
+    }
+
+    /**
+     * Determine if the user can create new groups with other users
+     */
+    public function canCreateChats(): bool
+    {
+        return $this->hasVerifiedEmail() == false;
     }
 }
